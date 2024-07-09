0360399df77502828e12ad00fdc1a5130aa110d9
Iryna Howarth <irynahowarth@gmail.com>
2024-07-09 07:13:41 +0100

    [feature]: add Navbar with logo and Menu/Avatar

---
+++ components/Navbar.tsx
@@ -0,0 +1,46 @@
+import Link from "next/link";
+import Image from 'next/image';
+import logo from '../img/logo.png'
+import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar'
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+  } from "@/components/ui/dropdown-menu"
+  
+
+const Navbar = () => {
+    return ( 
+    <div className="bg-primary dark:bg-slate-700 py-2 px-5 flex justify-between text-white">
+        <Link href='/'>
+            <Image src={logo} alt='Dashboard' width={40}/>
+        </Link>
+        
+
+        <DropdownMenu>
+            <DropdownMenuTrigger className="focus:outline-none">
+                <Avatar>
+                <AvatarImage src='https://github.com/shadcn.png' alt='@shadcn'/>
+                <AvatarFallback className="text-black">IH</AvatarFallback>
+                </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>
+                    <Link href='/profile'>Profile</Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                    <Link href='/auth'>Logout</Link>
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+
+
+    </div> );
+}
+ 
+export default Navbar;
