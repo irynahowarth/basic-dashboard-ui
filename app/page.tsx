82c05da2ab6c25539b48ba5d53ab09b7145edde4
Iryna Howarth <irynahowarth@gmail.com>
2024-07-10 14:42:58 +0100

    [feature]: add DashboardCard component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
 import Image from "next/image";
+import DashboardCard from "@/components/dashboard/DashboardCard";
+import { Folder, MessageCircle, Newspaper, User } from "lucide-react";
 
 export default function Home() {
   return (
   <>
-    <h1 className="text-2xl">Dashboard</h1>
+    <div className="flex flex-col md:flex-row justify-between gap-5 mb-5">
+      <DashboardCard title='Post' count={101} icon={<Newspaper className="text-slate-500" size={72}/>}/>
+      <DashboardCard title='Categories' count={11} icon={<Folder className="text-slate-500" size={72}/>}/>
+      <DashboardCard title='Users' count={803} icon={<User className="text-slate-500" size={72}/>}/>
+      <DashboardCard title='Comments' count={2993} icon={<MessageCircle className="text-slate-500" size={72}/>}/>
+    </div>
   </>
   );
 }
