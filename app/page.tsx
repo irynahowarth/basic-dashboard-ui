7c42b20401fb94583152774790e1c589ceab13ce
Iryna Howarth <irynahowarth@gmail.com>
2024-07-11 14:21:48 +0100

    [feature]: add PostTable

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import DashboardCard from "@/components/dashboard/DashboardCard";
 import { Folder, MessageCircle, Newspaper, User } from "lucide-react";
+import PostTable from "@/components/posts/PostTable";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <DashboardCard title='Users' count={803} icon={<User className="text-slate-500" size={72}/>}/>
       <DashboardCard title='Comments' count={2993} icon={<MessageCircle className="text-slate-500" size={72}/>}/>
     </div>
+    <PostTable limit={5}/>
   </>
   );
 }
